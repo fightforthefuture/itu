69cd95f0594a08c3b7165ab71b622888d22582f0
Chuck Harmston <chuck@chuckharmston.com>
2012-11-19 12:14:58 -0600

    Turns on DEBUG in staging

--- project/settings/staging.py
+++ project/settings/staging.py
@@ -6,6 +6,8 @@ INSTALLED_APPS = list(INSTALLED_APPS) + [
     'debug_toolbar',
 ]
 
+DEBUG = TEMPLATE_DEBUG = True
+
 AWS_STORAGE_BUCKET_NAME = 'fftf.itu.staging'
 AWS_S3_CUSTOM_DOMAIN = "s3.amazonaws.com/fftf.itu.staging"
 STATIC_URL = 'https://s3.amazonaws.com/fftf.itu.staging/'
